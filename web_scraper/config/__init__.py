0a118bd9885d59487a8af0ad8cb76ad17b70e128
DDAaroussel <alex@deepdiveanalytics.com.au>
2020-04-15 23:15:17 +0200

    adding module that fetches and loads all data to s3

--- web_scraper/config/__init__.py
+++ web_scraper/config/__init__.py
@@ -1,9 +1,9 @@
-import ConfigParser
+import configparser
 import os
 
-print os.path.dirname(os.path.realpath(__file__))
+print(os.path.dirname(os.path.realpath(__file__)))
 
 configs_dir = os.path.dirname(os.path.realpath(__file__))
-s3_settings = ConfigParser.ConfigParser()
+s3_settings = configparser.ConfigParser()
 
 s3_settings.read(['%s/s3_settings.ini' % configs_dir])
---
+++ web_scraper/lambda_function.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import boto3
+from config import s3_settings  # this is only created by by init when this file is run.
+import requests
+from bs4 import BeautifulSoup
+import json
+import pickle
+import os
+import logging
+
+logging.basicConfig(level='INFO')
+
+# get AWS credentials and create s3 resource
+secret_access_key = s3_settings.get('aws', 'aws_secret_access_key')
+access_key_id = s3_settings.get('aws', 'aws_access_key_id')
+s3 = boto3.client('s3', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
+
+
+def generate_valid_urls():
+    try:
+        with open("valid_urls.txt", "rb") as fp:
+            result_list = pickle.load(fp)
+    except FileNotFoundError:
+        print("File not found. Generating list from HDX URLs. This may take a few minutes.")
+        iso_dict = {}
+        result_list = []
+        with open('iso3.json') as json_file:
+            iso_list = json.load(json_file)
+        for i in iso_list:
+            iso_dict.update({i['name']: i['iso3']})
+        parent_url_root = 'https://data.humdata.org/dataset/acled-data-for-'
+        for x in iso_dict.keys():
+            if requests.head(parent_url_root + x).status_code == 200:
+                result_list.append(parent_url_root + x)
+    return result_list
+
+
+def download_to_s3(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    a = soup.find("a", class_="btn btn-empty btn-empty-blue hdx-btn resource-url-analytics ga-download")
+    b = a.get('href')
+    url_root = "https://data.humdata.org"
+    file_name = b.rsplit('/', 1)[1]
+    try:
+        csv_df = pd.read_csv(url_root + b, header=2)
+    except ConnectionError:
+        logging.error("CSV download failed.")
+    else:
+        csv_df.to_csv('data/{}'.format(file_name))
+    try:
+        s3.upload_file('data/{}'.format(file_name), 'aroussel-dev',
+                       'un_data/{}'.format(file_name))
+    except ConnectionError:
+        logging.error("Upload to S3 failed.")
+    else:
+        os.remove('data/{}'.format(file_name))
+
+
+def lambda_handler(event, context):
+
+    valid_urls = generate_valid_urls()
+
+    download_to_s3(valid_urls[1])
+
+    return 0
