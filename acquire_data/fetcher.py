638a6ed4b95f24377d08e758329787c599e856f2
DDAaroussel <alex@deepdiveanalytics.com.au>
2020-04-29 10:09:05 +0200

    adding function to check whether file in s3 is latest version

--- acquire_data/fetcher.py
+++ acquire_data/fetcher.py
@@ -9,6 +9,8 @@ import pickle
 import os
 import logging
 import wget
+import pytz
+from datetime import datetime
 
 logging.basicConfig(level='INFO')
 
@@ -18,7 +20,7 @@ def generate_valid_urls():
         with open("valid_urls.txt", "rb") as fp:
             result_list = pickle.load(fp)
     except FileNotFoundError:
-        logging.warn("File not found. Generating list from HDX URLs. This may take a few minutes.")
+        logging.warning("File not found. Generating list from HDX URLs. This may take a few minutes.")
         iso_dict = {}
         result_list = []
         with open('iso3.json') as json_file:
@@ -33,18 +35,18 @@ def generate_valid_urls():
 
 
 def download_to_s3(url, client, session):
-    page = session.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    a = soup.find(
-        "a", class_="btn btn-empty btn-empty-blue hdx-btn resource-url-analytics ga-download")
-    b = a.get('href')
-    url_root = "https://data.humdata.org"
-    file_name = b.rsplit('/', 1)[1]
-
+    full_url, file_name, latest_upload_date = get_page_info(url, session)
     if len(file_name) == 21:
         try:
             obj = client.head_object(Bucket='aroussel-dev', Key='data/{}'.format(file_name))
-            logging.info("{} already exists in s3. Not uploading.".format(file_name))
+            logging.info("{} already exists in s3. Checking to see if it is up to date.".format(file_name))
+            last_s3_upload = obj['LastModified']
+            if last_s3_upload < latest_upload_date:
+                logging.info("{} in s3 out of date, re-downloading and uploading".format(file_name))
+                csv = wget.download(full_url, out='data/')
+                client.upload_file('data/{}'.format(file_name), 'aroussel-dev', 'data/{}'.format(file_name))
+            else:
+                logging.info("{} in s3 up to date, no refresh needed".format(file_name))
         except ClientError as exc:
             if exc.response['Error']['Code'] == '404':
                 logging.info("{} does not exist in s3. Uploading to s3".format(file_name))
@@ -53,10 +55,27 @@ def download_to_s3(url, client, session):
                     client.upload_file('data/{}'.format(file_name), 'aroussel-dev', 'data/{}'.format(file_name))
                 else:
                     logging.info("{} does not exist locally, downloading then uploading to s3".format(file_name))
-                    csv = wget.download(url_root + b, out='data/')
+                    csv = wget.download(full_url, out='data/')
                     client.upload_file('data/{}'.format(file_name), 'aroussel-dev', 'data/{}'.format(file_name))
     elif len(file_name) == 21:
-        logging.warn("{} is not a valid file name and was not downloaded".format(file_name))
+        logging.warning("{} is not a valid file name and was not downloaded".format(file_name))
+
+
+def get_page_info(url, session):
+    page = session.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    a = soup.find(
+        "a", class_="btn btn-empty btn-empty-blue hdx-btn resource-url-analytics ga-download")
+    b = a.get('href')
+    url_root = "https://data.humdata.org"
+    full_url = url_root + b
+    file_name = b.rsplit('/', 1)[1]
+    latest_upload_date_search = soup.find("span", class_="date")
+    latest_upload_date_string = latest_upload_date_search.get('title').rsplit(',', 1)[0].replace(',', '')
+    utc = pytz.UTC
+    latest_upload_date = utc.localize(datetime.strptime(latest_upload_date_string, '%B %d %Y'))
+    return full_url, file_name, latest_upload_date
+
 
 def run():
     secret_access_key = s3_settings.get('aws', 'aws_secret_access_key')
@@ -69,6 +88,10 @@ def run():
         os.mkdir('data/')
     with requests.Session() as my_session:
         for url in valid_urls:
-            logging.info("downloading {}".format(url))
+            logging.info("checking URL: {}".format(url))
             download_to_s3(url, s3, my_session)
     return 0
+
+
+if __name__ == '__main__':
+    run()
